ca8412f8d587e8d408c9b63bcd5a12dcd8d7cd48
Lily Lu <lilyzhaoyilu@gmail.com>
2022-07-19 22:50:32 -0700

    Update query error handling logic
    Now handling error cases when:
    1. querying in CN, not supported
    2. query return not found with code 200 or 404
    3. query return 403 exceeding limit
    4. query return others

--- src/commands/apex/apex_query.ts
+++ src/commands/apex/apex_query.ts
@@ -14,22 +14,78 @@ class ApexQuery extends AppCommand {
 
     const curUser = session.user.nickname || session.user.username;
     const queryUser = session.args[0];
-    await getApexAccountStatus(queryUser).then(res => {
+    await axios.get(`https://api.mozambiquehe.re/bridge?auth=${auth.apexTracker}&player=${queryUser}&platform=PC`).then((res: any) => {
+
+      // res might return 200 when not finding user data
       if (res.data.Error) {
-        session.client.API.message.create(9, '9682242694390929', `Apex query not found: currentUser ${curUser}#${session.user.identifyNum} querying ${queryUser}`);
-        return session.sendCard(notFoundCardConstructor(curUser, queryUser));
+        session.client.API.message.create(9, '9682242694390929', `:grey_question: Apex query notfound: currentUser ${curUser}#${session.user.identifyNum} has error: ${res.data.Error} when querying: ${queryUser}`);
+        return session.sendCard(constructNotFoundCard(curUser, queryUser));
       }
-      const data = res.data;
-      return session.sendCard(constructCard(curUser, data));
-    }, rej => {
-      console.log('Apex query error: send by', curUser, ' querying', queryUser, 'rej reason: ', rej, 'Time: ', new Date().toLocaleString('en-US', { timeZone: 'America/Los_Angeles' }));
-      session.client.API.message.create(9, '9682242694390929', `Apex query error: currentUser ${curUser}#${session.user.identifyNum} has error: ${rej}`);
-      return session.sendCard(constructErrorCard(curUser));
+      // send back query result
+      return session.sendCard(constructCard(curUser, res.data));
+    }).catch((err: any) => {
+
+      if (!axios.isAxiosError(err)) {
+
+        // Handle query in Chinese
+        if (err.code === 'ERR_UNESCAPED_CHARACTERS') {
+          return session.quote('暂时不支持对中文ID的查询。如果你的Origin ID是中文的，请私信狗头这个问题。提交真的bug，狗头有小红包给你哦~');
+        }
+
+        console.error(`ERROR at ${new Date().toLocaleString('en-US', { timeZone: 'America/Los_Angeles' })} in **QUERY**: ${err}`)
+        return session.sendCard(constructErrorCard(curUser));
+      }
+      switch (err.response.status) {
+        // did not find data
+        case 404:
+          session.client.API.message.create(9, '9682242694390929', `:grey_question: Apex query notfound: currentUser ${curUser}#${session.user.identifyNum} has error: ${err} when querying: ${queryUser}`);
+          return session.sendCard(constructNotFoundCard(curUser, queryUser));
+          break;
+        // over rate limit
+        case 429:
+          session.client.API.message.create(9, '9682242694390929', `:no_entry: Apex query overlimit: currentUser ${curUser}#${session.user.identifyNum} has error: ${err} when querying: ${queryUser}`);
+          return session.sendCard(constructOverlimitCard(curUser));
+          break;
+        default:
+          session.client.API.message.create(9, '9682242694390929', `:no_entry: Apex query error: currentUser ${curUser}#${session.user.identifyNum} has error: ${err} when querying: ${queryUser}`);
+          return session.sendCard(constructErrorCard(curUser));
+      }
+
     });
+
+
+
+    // then((res: any) => {
+    //   console.log('corgi hit res');
+    //   console.log('corgi res', typeof res, ' ***: ', res);
+
+    //   console.log(res.data); //data
+    //   console.log('--------')
+    //   console.log(res.status); //200
+    //   console.log('--------')
+    //   console.log(res.statusText); //OK
+    //   console.log('--------')
+    //   console.log(res.headers);
+    //   console.log('--------')
+    //   console.log(res.config);
+    //   console.log('--------')
+    // }).catch((err: any) => {
+    //   console.log('corgi hit err');
+    //   console.log('corgi err', typeof err, ' ***: ', err);
+    //   console.log('--------')
+    //   console.log('data: ', err.response.data); // {error: ...}
+    //   console.log('--------')
+    //   console.log('stat: ', err.response.status); //404
+    //   console.log('--------')
+    //   console.log('header: ', err.response.headers);
+    //   console.log('--------')
+    // });
+
   }
 };
 
-const notFoundCardConstructor = (curUser: string, queryUser: string) => {
+
+const constructNotFoundCard = (curUser: string, queryUser: string) => {
   return `[
     {
       "type": "card",
@@ -50,14 +106,14 @@ const notFoundCardConstructor = (curUser: string, queryUser: string) => {
           "type": "section",
           "text": {
             "type": "kmarkdown",
-            "content": "没有查询到关于${queryUser}的数据，目前只支持用Origin平台的ID进行查询。"
+            "content": "没有查询到关于 ${queryUser} 的数据，目前只支持用Origin平台的ID进行查询。"
           }
         },
         {
           "type": "section",
           "text": {
             "type": "kmarkdown",
-            "content": "如果是第一次使用查询功能，请去 [apexlegendsstatus.com](https://apexlegendsstatus.com/stats/) 查一下自己的橘子id，让自己的 id 进入到数据库中（绿色箭头）。请确保选择PC平台（黄色箭头）。"
+            "content": "如果是第一次使用查询功能，请去 [apexlegendsstatus.com](https://apexlegendsstatus.com/stats/) 查一下自己的橘子ID，让自己的 ID 进入到数据库中（绿色箭头）。请确保选择PC平台（黄色箭头）。"
           }
         },
         {
@@ -95,7 +151,7 @@ const constructErrorCard = (curUser: string) => {
           "type": "section",
           "text": {
             "type": "kmarkdown",
-            "content": "同一时间查询的次数超过了服务器限制，请再试一下。如果你觉得这是一个bug，可以私信狗头。非常感谢你的帮助:blush:"
+            "content": "查询出现了错误，狗头已经记录了这个错误，会在周末的时候进行维修和升级。:computer:"
           }
         }
       ]
@@ -103,9 +159,35 @@ const constructErrorCard = (curUser: string) => {
   ]`
 }
 
-const getApexAccountStatus = async (queryUser: string) => {
-  return axios.get(`https://api.mozambiquehe.re/bridge?auth=${auth.apexTracker}&player=${queryUser}&platform=PC`);
+const constructOverlimitCard = (curUser: string) => {
+  return `[
+    {
+      "type": "card",
+      "theme": "secondary",
+      "size": "lg",
+      "modules": [
+        {
+          "type": "header",
+          "text": {
+            "type": "plain-text",
+            "content": "${curUser}的查询结果"
+          }
+        },
+        {
+          "type": "divider"
+        },
+        {
+          "type": "section",
+          "text": {
+            "type": "kmarkdown",
+            "content": "同一时间查询的次数超过了服务器限制，请再试一下。如果你觉得这是一个bug，可以私信狗头。对于确实存在的bug，狗头有小红包给你哦~:blush:"
+          }
+        }
+      ]
+    }
+  ]`
 }
+
 
 const translateRanking = (rank: string) => {
   switch (rank) {
